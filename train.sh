848f652e5cdea3a9b90ddc36db1da565664b11e5
Zhimeng Guo <gzjz07@outlook.com>
2024-09-17 20:51:30 -0400

    new bash

--- train.sh
+++ train.sh
@@ -1,18 +1,20 @@
-export CUDA_VISIBLE_DEVICES=2
-export OMP_NUM_THREADS=1
 
-export MODEL_NAME="CompVis/stable-diffusion-v1-4"
-export DATASET_NAME="lambdalabs/naruto-blip-captions"
+#!/bin/bash
 
-# accelerate launch --mixed_precision="fp16"  --num_processes 1 --main_process_port=29292 \
-python \
-    main.py \
-  --pretrained_model_name_or_path="CompVis/stable-diffusion-v1-4" \
-  --dataset_name="lambdalabs/naruto-blip-captions" \
+
+DEFAULT_COMMAND="python main.py \
+  --pretrained_model_name_or_path=\"CompVis/stable-diffusion-v1-4\" \
+  --dataset_name=\"lambdalabs/naruto-blip-captions\" \
   --resolution=512 --center_crop --random_flip \
-  --train_batch_size=32 \
-  --max_train_steps=150000000 \
+  --train_batch_size=4 \
+  --max_train_steps=1000000000 \
   --learning_rate=1e-05 \
   --max_grad_norm=1 \
-  --lr_scheduler="constant" --lr_warmup_steps=0 \
-  --output_dir="output"
+  --lr_scheduler=\"constant\" --lr_warmup_steps=0 \
+  --output_dir=\"output\""
+
+export DEFAULT_COMMAND
+# export OMP_NUM_THREADS=1
+
+
+python train.py -p "2,2" "3,1"
