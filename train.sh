828645cd4281f9c3d799c61ae46214e276086d7a
Zhimeng Guo <gzjz07@outlook.com>
2024-09-17 21:49:43 -0400

    some

--- train.sh
+++ train.sh
@@ -1,20 +1,4 @@
-
-#!/bin/bash
-
-
-DEFAULT_COMMAND="python main.py \
-  --pretrained_model_name_or_path=\"CompVis/stable-diffusion-v1-4\" \
-  --dataset_name=\"lambdalabs/naruto-blip-captions\" \
-  --resolution=512 --center_crop --random_flip \
-  --train_batch_size=64 \
-  --learning_rate=1e-05 \
-  --max_grad_norm=1 \
-  --lr_scheduler=\"constant\" --lr_warmup_steps=0 \
-  --output_dir=\"output\""
-
-export DEFAULT_COMMAND
 export OMP_NUM_THREADS=1
 
 # each 1 18g 
-# python running.py -p "1,1" "2,1" "3,1" "4,1" "5,1"
 python running.py -p 4 7 -b 64 64
